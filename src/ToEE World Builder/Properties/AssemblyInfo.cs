a9c134a9fd7e8d137a0f0f7a29365a7c60e20ca1
13xforever <ilya.veselov@gmail.com>
2013-05-05 03:29:01 +0600

    Made new icon. Hopefully no one will tell the difference.

--- src/ToEE World Builder/Properties/AssemblyInfo.cs
+++ src/ToEE World Builder/Properties/AssemblyInfo.cs
@@ -1,21 +1,4 @@
-// ToEE World Builder .NET2 version 2.0.0 Open-Source Edition
-// Copyright (C) 2005-2006    Michael Kamensky, all rights reserved.
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
 //
@@ -28,7 +11,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ToEE World Builder")]
-[assembly: AssemblyCopyright("� 2005-2013 Michael Kamensky")]
+[assembly: AssemblyCopyright("© 2005-2013 Michael Kamensky")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
