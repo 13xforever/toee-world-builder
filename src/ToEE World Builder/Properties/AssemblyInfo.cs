c89a59912f0bbd75ae58b9775e56429d2ce659e6
13xforever <ilya.veselov@gmail.com>
2013-05-04 18:08:32 +0600

    Removed all the clutter

--- src/ToEE World Builder/Properties/AssemblyInfo.cs
+++ src/ToEE World Builder/Properties/AssemblyInfo.cs
@@ -23,12 +23,12 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("")]
+[assembly: AssemblyTitle("ToEE World Builder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyProduct("ToEE World Builder")]
+[assembly: AssemblyCopyright("© 2005-2013 Michael Kamensky")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
@@ -43,7 +43,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.5.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
